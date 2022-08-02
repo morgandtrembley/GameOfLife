4404c8881d0dbe53db316fe6764a324d9045be7d
Morgan <morgandtrembley@gmail.com>
2022-08-01 21:07:33 -0700

    Refactored image functions and added toggle for image generation.

--- GameOfLife.cpp
+++ GameOfLife.cpp
@@ -1,14 +1,15 @@
-#pragma warning(disable : 4996)
+/*	Conways Game of Life with support for 64-bit cell coordinates.	*/
+#pragma warning(disable : 4996)				// ctime warnings via boost
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <unordered_set>
 #include <unordered_map>
 #include <chrono>
-#include <boost/functional/hash/hash.hpp>	// pair hashing
-#include <boost/gil.hpp>					// image support
-#include <boost/gil/io/io.hpp>				// image support
-#include <boost/gil/extension/io/bmp.hpp>	// bmp write support
+#include <boost/gil.hpp>					// image editing
+#include <boost/gil/io/io.hpp>				// ^^
+#include <boost/gil/extension/io/bmp.hpp>	// .bmp write
+#include <boost/functional/hash/hash.hpp>	// int pair hashing
 
 #define BORN 1
 #define DIED -1
@@ -25,26 +26,22 @@ struct cellStruct {
 	int neighbors = 0;
 };
 
-// ---------- Edit Number of Generations Here
 const int generations = 10;
 
-
-
-// Edit Image Parameters Here
-
+// ---------- Edit Image Parameters Here ----------
+const bool genImages = false;	// set to true to generate image representations of each generation
 const int cellWidth = 6;	// in pixels (even # for best results)
-
 const int imgSizeX = 55;	// in cells (odd for best results)
 const int imgSizeY = 55;	// in cells (odd for best results)
 
-// image center coords: image CAN NOT EXCEED int64 Bounds! imgCenter will be moved if out of range
-pair imgCenter = pair(-15,4);
+// image center cell coordinates
+pair imgCenter = pair(-4,7);
 
 // image Colors
 color darkGrey(10, 10, 10);
 color medGrey(25, 25, 25);
 color lightGrey(50, 50, 50);
-color green(0, 100, 0);
+color green(0, 255, 0);
 color red(150, 0, 0);
 color blue(0, 0, 100);
 
@@ -60,7 +57,6 @@ bool GameRules(const cellStruct& cell) {
 	return false;
 }
 
-// wrap around coordinate system
 void GetNeighborCoordinates(int64_t *cols, int64_t *rows, const pair& coords) {
 	// center|left|right
 	cols[0] = coords.first;
@@ -115,111 +111,109 @@ void CalculateNextGeneration(umap& board, uset& changes) {
 
 // ---------- Image Processing ----------
 
-void DrawCell(image& img, pair coords) {
-	int imageCenter = (img.height() / 2) * img.width() + img.width() / 2;
-	int centerShift = (imgCenter.second * img.width() - imgCenter.first) * cellWidth;
-	int coordinateShift = (coords.first - coords.second * img.width()) * cellWidth;
+void DrawCell(image& snapshot, pair coords) {
+	int imageCenter = (snapshot.height() / 2) * snapshot.width() + snapshot.width() / 2;
+	int centerShift = (imgCenter.second * snapshot.width() - imgCenter.first) * cellWidth;
+	int coordinateShift = (coords.first - coords.second * snapshot.width()) * cellWidth;
 	for (int i = 0; i < cellWidth - 1; i++) {
-		for (int j = 0; j < cellWidth - 1; j++) {			
-			int cellShift = (1 + i - cellWidth / 2) * img.width() + j - cellWidth / 2 + 1;
-			view(img)[imageCenter + centerShift + coordinateShift + cellShift] = red;
+		for (int j = 0; j < cellWidth - 1; j++) {		
+			int cellShift = (1 + i - cellWidth / 2) * snapshot.width() + j - cellWidth / 2 + 1;
+			view(snapshot)[imageCenter + centerShift + coordinateShift + cellShift] = red;
 		}
 	}
 }
 
 bool InRange(const pair& coords, const pair& minRange, const pair& maxRange) {
-	if (coords.first >= minRange.first && coords.first <= maxRange.first &&
-			coords.second >= minRange.second && coords.second <= maxRange.second) {
-		return true;
-	}
-	return false;
+	bool condition1 = coords.first >= minRange.first && coords.first <= maxRange.first;
+	bool condition2 = coords.second >= minRange.second && coords.second <= maxRange.second;
+	return (condition1 && condition2) ? true : false;
 }
 
-void DrawImage(const umap& board, const image img, const pair& minRange, const pair& maxRange, const std::string tag) {
-	image newImg = img;
+void DrawImage(const umap& board, const image base, const pair& minRange, const pair& maxRange, const std::string tag) {
+	image snapshot = base;
 	for (auto& cell:board) {
 		if (cell.second.state) {
 			if (InRange(cell.first, minRange, maxRange)) {
-				DrawCell(newImg, cell.first);
+				DrawCell(snapshot, cell.first);
 			}
 		}
 	}
-	boost::gil::write_view("images/img" + tag + ".bmp", view(newImg), boost::gil::bmp_tag());
+	boost::gil::write_view("images/generation" + tag + ".bmp", view(snapshot), boost::gil::bmp_tag());
 }
 
-void DrawBounds(image& img, const pair& minRange, const pair& maxRange, const int range) {
+void DrawBounds(image& base, const pair& minRange, const pair& maxRange, const int range) {
 	// lower boundary
 	if (minRange.second == INT64_MIN) {
-		for (int y = img.height() * (img.width() - 1); y < img.height() * img.width(); y++) {
-			view(img)[y] = blue;
+		for (int y = base.height() * (base.width() - 1); y < base.height() * base.width(); y++) {
+			view(base)[y] = blue;
 		}
 	}
 	// upper boundary
 	if (maxRange.second == INT64_MAX) {
-		for (int y = 0; y < img.width(); y++) {
-			view(img)[y] = blue;
+		for (int y = 0; y < base.width(); y++) {
+			view(base)[y] = blue;
 		}
 	}
 	// left boundary
 	if (minRange.first == INT64_MIN) {
-		for (int x = 0; x < range; x += img.width()) {
-			view(img)[x] = blue;
+		for (int x = 0; x < range; x += base.width()) {
+			view(base)[x] = blue;
 		}
 	}
 	// right boundary
 	if (maxRange.first == INT64_MAX) {
-		for (int x = img.width() - 1; x < range; x += img.width()) {
-			view(img)[x] = blue;
+		for (int x = base.width() - 1; x < range; x += base.width()) {
+			view(base)[x] = blue;
 		}
 	}
 }
 
-void DrawZeroes(image& img, const pair& minRange, const pair& maxRange, const int range) {
+void DrawZeroes(image& base, const pair& minRange, const pair& maxRange, const int range) {
 	// line x = 0
 	if (minRange.first < 0 && maxRange.first > 0) {
-		int offset = img.width() / 2 - imgCenter.first * cellWidth;
-		for (int x = offset; x < range; x += img.width()) {
-			view(img)[x] = green;
+		int offset = base.width() / 2 - imgCenter.first * cellWidth;
+		for (int x = offset; x < range; x += base.width()) {
+			view(base)[x] = green;
 		}
 	}
 	// line y = 0
 	if (minRange.second < 0 && maxRange.second > 0) {
-		int offset = (img.height() / 2 + imgCenter.second * cellWidth) * img.width();
-		for (int y = 0; y < img.width(); y++) {
-			view(img)[offset + y] = green;
+		int offset = (base.height() / 2 + imgCenter.second * cellWidth) * base.width();
+		for (int y = 0; y < base.width(); y++) {
+			view(base)[offset + y] = green;
 		}
 	}
 }
 
-void DrawGridlines(image& img, const int range) {
+void DrawGridlines(image& base, const int range) {
 	// Vertical
-	for (int x = 0; x < range; x += img.width()) {
-		for (int y = 0; y < img.width(); y += cellWidth) {
-			view(img)[x + y] = medGrey;
+	for (int x = 0; x < range; x += base.width()) {
+		for (int y = 0; y < base.width(); y += cellWidth) {
+			view(base)[x + y] = medGrey;
 		}
 	}
 	// Horozontal
-	int stride = cellWidth * img.width();
+	int stride = cellWidth * base.width();
 	for (int x = 0; x < range; x += stride) {
-		for (int y = 0; y < img.width(); y++) {
+		for (int y = 0; y < base.width(); y++) {
 			// if overlapping make cell brighter
-			if (view(img)[x + y] == medGrey) {
-				view(img)[x + y] = lightGrey;
+			if (view(base)[x + y] == medGrey) {
+				view(base)[x + y] = lightGrey;
 			}
 			else {
-				view(img)[x + y] = medGrey;
+				view(base)[x + y] = medGrey;
 			}
 		}
 	}
 }
 
-void GenerateBaseImage(image& img, const pair& minRange, const pair& maxRange) {
-	int range = img.height() * img.width();
+void GenerateBaseImage(image& base, const pair& minRange, const pair& maxRange) {
+	int range = base.height() * base.width();
 
-	fill_pixels(view(img), darkGrey);
-	DrawGridlines(img, range);
-	DrawZeroes(img, minRange, maxRange, range);
-	DrawBounds(img, minRange, maxRange, range);
+	fill_pixels(view(base), darkGrey);
+	DrawGridlines(base, range);
+	DrawZeroes(base, minRange, maxRange, range);
+	DrawBounds(base, minRange, maxRange, range);
 }
 
 void GenerateCoordinateRange(pair& minRange, pair& maxRange) {
@@ -229,7 +223,7 @@ void GenerateCoordinateRange(pair& minRange, pair& maxRange) {
 	maxRange.second = imgCenter.second + imgSizeY/2;
 }
 
-void ValidateImageCenter() {
+void ClampImageCenter() {
 	if (imgCenter.first > 0) {
 		if (INT64_MAX - imgSizeX/2 <= imgCenter.first) {
 			imgCenter.first = INT64_MAX - imgSizeX / 2;
@@ -251,6 +245,8 @@ void ValidateImageCenter() {
 		}
 	}
 }
+
+// ---------- Input/Output ----------
 
 void ReadTestInput(umap& board, uset& changes) {
 	pair coords;
@@ -278,32 +274,27 @@ void WriteResults(const umap& board) {
 	results.close();
 }
 
+// ---------- Main ----------
+
 int main() {
 	uset changes;
 	umap board;
 	pair minRange, MaxRange;
-
-
-	image img(imgSizeX * cellWidth + 1, imgSizeY * cellWidth + 1);
-	ValidateImageCenter();
-	GenerateCoordinateRange(minRange,MaxRange);
-	GenerateBaseImage(img, minRange, MaxRange);
+	image base(imgSizeX * cellWidth + 1, imgSizeY * cellWidth + 1);
 
 	ReadTestInput(board, changes);
 
-	std::chrono::time_point<std::chrono::system_clock> start, end;
-	start = std::chrono::system_clock::now();
+	if (genImages) {
+		ClampImageCenter();
+		GenerateCoordinateRange(minRange, MaxRange);
+		GenerateBaseImage(base, minRange, MaxRange);
+	}
 
 	for (int i = 0; i < generations; ++i) {
-		DrawImage(board, img, minRange, MaxRange, std::to_string(i));
+		if (genImages) { DrawImage(board, base, minRange, MaxRange, std::to_string(i)); }
 		CalculateNextGeneration(board, changes);
 	}
-	DrawImage(board, img, minRange, MaxRange, "Final");
-
-
-	end = std::chrono::system_clock::now();
-	std::chrono::duration<double> runtime = end - start;
-	std::cout << "Runtime: " << runtime.count() << " seconds\n";
+	if (genImages) { DrawImage(board, base, minRange, MaxRange, "Final"); }
 
 	WriteResults(board);
 
